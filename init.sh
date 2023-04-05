865846f2ff2432af565d23e2c2762cc402d178f8
Alexander Sabanov <alexsab@mail.ru>
2023-04-05 16:22:21 +0700

    Update init.sh
    for MacOS

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function get_dc_env() {
-  sed -n -e "/${1}/ s/.*\= *//p" .env
+  gsed -n -e "/${1}/ s/.*\= *//p" .env
 }
 
 function fill_placeholder() {
-  sed -ri "s~$1~$2~" .env
+  gsed -ri "s~$1~$2~" .env
 }
 
 cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" || exit
@@ -13,8 +13,9 @@ ENV_FILE=.env
 #Check if env file exists and offer to recreate it
 if [ -f "$ENV_FILE" ]; then
   echo "The .env file already exists."
-  read -r -p "Would you like to remove it? (Y|N): " -i 'N' -e REMOVE_ENV_FILE
-  if [ "${REMOVE_ENV_FILE^^}" == 'Y' ]; then
+  echo -n "Would you like to remove it? (Y|N): "
+  read -r -e REMOVE_ENV_FILE
+  if [ "$(echo "$REMOVE_ENV_FILE" | tr '[:lower:]' '[:upper:]')" == 'Y' ]; then
     rm .env
     cp .env.template .env
   fi
@@ -27,7 +28,9 @@ CURRENT_DIR=$PWD
 PARENT_DIR=$(dirname "$PWD")
 
 if grep -q @PATH_TO_PROJECT@ ".env"; then
-  read -r -p "Type the project path: " -i "$PARENT_DIR" -e PROJECT_PATH
+  echo -n "Type the project path: [$PARENT_DIR] "
+  read -r -e PROJECT_PATH
+  PROJECT_PATH=${PROJECT_PATH:-$PARENT_DIR}
   fill_placeholder "@PATH_TO_PROJECT@" "$PROJECT_PATH"
 else
   PROJECT_PATH=$(get_dc_env PROJECT_PATH)
@@ -36,28 +39,40 @@ fi
 COMPOSE_PROJECT_NAME=${PROJECT_PATH%*/}
 
 if grep -q @YOUR_PROJECT_NAME@ ".env"; then
-  read -r -p "Type the project name: " -i "$(sed s~-~_~g <<<${COMPOSE_PROJECT_NAME##*/})" -e COMPOSE_PROJECT_NAME
+  DEFAULT_PROJECT_NAME=$(gsed s~-~_~g <<<${COMPOSE_PROJECT_NAME##*/})
+  echo -n "Type the project name: [$DEFAULT_PROJECT_NAME] "
+  read -r -e COMPOSE_PROJECT_NAME
+  COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME:-$DEFAULT_PROJECT_NAME}
   fill_placeholder "@YOUR_PROJECT_NAME@" "$COMPOSE_PROJECT_NAME"
 else
   COMPOSE_PROJECT_NAME=$(get_dc_env COMPOSE_PROJECT_NAME)
 fi
 
 if grep -q @YOUR_PROJECT_DOMAIN@ ".env"; then
-  read -r -p "Type the domain for project: " -i "${COMPOSE_PROJECT_NAME}.loc" -e PROJECT_DOMAIN
+  DEFAULT_DOMAIN="${COMPOSE_PROJECT_NAME}.loc"
+  echo -n "Type the domain for project: [$DEFAULT_DOMAIN] "
+  read -r -e PROJECT_DOMAIN
+  PROJECT_DOMAIN=${PROJECT_DOMAIN:-$DEFAULT_DOMAIN}
   fill_placeholder "@YOUR_PROJECT_DOMAIN@" "$PROJECT_DOMAIN"
 else
   PROJECT_DOMAIN=$(get_dc_env DOMAIN)
 fi
 
 if grep -q @PATH_TO_DOCKER_DATA@ ".env"; then
-  read -r -p "Type the path to docker data: " -i "$CURRENT_DIR/data" -e DATA_PATH
+  DEFAULT_DATA_PATH="$CURRENT_DIR/data"
+  echo -n "Type the path to docker data: [$DEFAULT_DATA_PATH] "
+  read -r -e DATA_PATH
+  DATA_PATH=${DATA_PATH:-$DEFAULT_DATA_PATH}
   fill_placeholder "@PATH_TO_DOCKER_DATA@" "$DATA_PATH"
 else
   DATA_PATH=$(get_dc_env DATA_PATH)
 fi
 
 if grep -q @PATH_TO_LOGS@ ".env"; then
-  read -r -p "Type the log path: " -i "$CURRENT_DIR/logs" -e LOGS_PATH
+  DEFAULT_LOGS_PATH="$CURRENT_DIR/logs"
+  echo -n "Type the log path: [$DEFAULT_LOGS_PATH] "
+  read -r -e LOGS_PATH
+  LOGS_PATH=${LOGS_PATH:-$DEFAULT_LOGS_PATH}
   fill_placeholder "@PATH_TO_LOGS@" "$LOGS_PATH"
 else
   LOGS_PATH=$(get_dc_env LOGS_PATH)
@@ -73,13 +88,17 @@ done
 
 #Create public directory
 if [ ! -d "${PROJECT_PATH}/public" ]; then
-  read -r -p "Create public folder with index.php? " -i 'Y' -e CREATE_PUBLIC
-  if [ "${CREATE_PUBLIC^^}" == 'Y' ]; then
+  echo -n "Create public folder with index.php? [Y]: "
+  read -r -e CREATE_PUBLIC
+  CREATE_PUBLIC=${CREATE_PUBLIC:-'Y'}
+  if [ "$(echo "$CREATE_PUBLIC" | tr '[:lower:]' '[:upper:]')" == 'Y' ]; then
     mkdir "${PROJECT_PATH}/public" && echo '<?php phpinfo();' >"${PROJECT_PATH}/public/index.php"
   fi
 fi
 
-read -r -p 'Download bitrix_setup.php? ' -i 'Y' -e DBS
+echo -n 'Download bitrix_setup.php? [Y]: '
+read -r -e DBS
+DBS=${DBS:-'Y'}
 
 if [ "${DBS}" == 'Y' ]; then
   wget "https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php" -O "${PROJECT_PATH}/public/bitrixsetup.php" -nv
